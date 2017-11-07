6ec2fd6c6f86da0a2c8628728717a19e7975887c
myTerminal <team.terminal@gmail.com>
2017-11-06 20:32:30 -0600

    enable downloading of 'unlimited' repos instead of just 30

--- index.js
+++ index.js
@@ -2,42 +2,53 @@
 
 /* global require module process */
 
-var execSync = require("child_process").execSync,
-    fadedProgressbar = require("faded-progressbar"),
-    GitHubApi = require("github"),
+var execSync = require('child_process').execSync,
+    fadedProgressbar = require('faded-progressbar'),
+    GitHubApi = require('github'),
     github = new GitHubApi({
-        debug: true,
-        protocol: "https",
-        host: "api.github.com",
+        protocol: 'https',
+        host: 'api.github.com',
         headers: {
-            "user-agent": "git-getter"
+            'user-agent': 'git-getter'
         },
         Promise: require('bluebird'),
         followRedirects: false,
         timeout: 5000
     }),
     args = process.argv,
-    username = args[2];
-
-github.repos.getForUser(
-    {
-        username: username,
-        type: "owner"
+    username = args[2],
+    addResults = function (link, results, onDone) {
+        github.getNextPage(link, null, function (err, res) {
+            if (res.data.length == 30) {
+                addResults(res, results.concat(res.data), onDone);
+            } else {
+                onDone(results.concat(res.data));
+            }
+        })
     },
-    function(err, res) {
-        var sourceRepos = res.data.filter(r => !r.fork),
+    downloadRepos = function (repos) {
+        var sourceRepos = repos.filter(r => !r.fork),
             downloadProcess;
 
-        console.log("" + sourceRepos.length + " repositories found");
+        console.log('' + sourceRepos.length + ' repositories found');
 
-        downloadProcess = fadedProgressbar.newProcess("Downloading repositories", sourceRepos.length);
+        downloadProcess = fadedProgressbar.newProcess('Downloading repositories', sourceRepos.length);
         downloadProcess.start();
 
         sourceRepos.forEach((r, i) => {
-            execSync("git clone " + r.clone_url, {
+            execSync('git clone ' + r.clone_url, {
                 stdio: [0, 1, 2]
             });
             downloadProcess.updateProgress(i + 1);
         });
+    };
+
+github.repos.getForUser(
+    {
+        username: username,
+        type: 'owner'
+    },
+    function(err, res) {
+        addResults(res, res.data, downloadRepos);
     }
 );
